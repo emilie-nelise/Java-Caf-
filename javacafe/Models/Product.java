d23760c5e26048d6ff0697958b13919191a3b0d3
emilie-nelise <105816701+emilie-nelise@users.noreply.github.com>
2025-06-25 22:58:57 -0300

    Update Product.java

--- javacafe/Models/Product.java
+++ javacafe/Models/Product.java
@@ -1,21 +1,19 @@
 package javacafe.Models;
 
 public class Product {
-    private final String name; // O nome do produto (ex: "capuccino")
-    private int stock;      // A quantidade em estoque
+    private final String name;
+    private int stock;
+    private final double price; 
 
-    public Product(String name, int initialStock) {
+    public Product(String name, int initialStock, double price) { 
         this.name = name;
         this.stock = initialStock;
+        this.price = price; 
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getStock() {
-        return stock;
-    }
+    public String getName() { return name; }
+    public int getStock() { return stock; }
+    public double getPrice() { return price; } 
 
     public void updateStock(int amount) {
         this.stock += amount;
